bac98dfc509263ea9391eabdcd4a1654a88c98e7
jangkyoungmoo <jangkyoungmoo@naver.com>
2026-08-08 16:32:41 +0900

    ✨Feat: BottomNav.jsx 수정 완료

--- src/common/bottomNav/BottomNav.jsx
+++ src/common/bottomNav/BottomNav.jsx
@@ -1,8 +1,23 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Nav = styled.nav`
+position: absolute;
+bottom: 0;
+left: 0;
+right: 0;
+display: flex;
+justify-content: space-around;    
+`;
+
 function BottomNav() {
   return (
-    <div>
-      <h1>하단 네비게이션 화면입니다</h1>
-    </div>
+    <Nav>
+      <Link to="/">홈</Link>
+      <Link to="/feed">피드</Link>
+      <Link to="/market">마켓</Link>
+      <Link to="/mypage">마이</Link>
+    </Nav>
   );
 }
 

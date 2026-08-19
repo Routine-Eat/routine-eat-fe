520de64e45afa5f76323a3dbe8e29183848dc1db
Jeong mokjin <jungmokjin@gmail.com>
2026-08-19 11:04:32 +0900

    ✨ Feat: PillButton 아이콘 맵핑 기능 추가

---
+++ src/common/PillButton.jsx
@@ -0,0 +1,66 @@
+import styled from 'styled-components';
+
+import remove from '@/assets/onboarding/tools/remove.svg';
+import { getEquipmentIcon, getEtcIcon, getIngredientIcon } from '@/constants/iconsMap';
+
+export default function PillButton({
+  kind, // 종류 : INGREDIENT(식재료)/EQUIPMENT(조리도구)/ETC(기타)
+  detailType, // 각 종류의 대분류 (ex: 식재료-FRUIT/SUGAR... 조리도구-APPLIANCE/UTENSIL... 기타-필요 없음)
+  name, // 이름
+  amountValue, // 보유량 - 식재료에만 쓰일 예정
+  onClick, // 클릭 시 이벤트
+  isSelected, // 선택 됐는지 여부 (true/false)
+  deleteAvailable, // 제거 가능한지 여부 = X 버튼 표시 여부 (true/false)
+}) {
+  const iconUrl = (kind) => {
+    if (kind === 'INGREDIENT') {
+      return getIngredientIcon(name, detailType);
+    } else if (kind === 'EQUIPMENT') {
+      return getEquipmentIcon(name, detailType);
+    } else {
+      return getEtcIcon(name);
+    }
+  };
+  return (
+    <Case $isSelected={isSelected} onClick={onClick}>
+      {deleteAvailable && <XBtn src={remove} />}
+      <Icon src={iconUrl(kind)} />
+      {name}
+      <Amount>{amountValue}</Amount>
+    </Case>
+  );
+}
+
+const Case = styled.button`
+  display: flex;
+  padding: 8px 16px;
+  justify-content: center;
+  align-items: center;
+  gap: 6px;
+  border-radius: 30px;
+  background: ${({ $isSelected }) => ($isSelected ? '#D6F3A1' : 'white')};
+  border: ${({ $isSelected }) => ($isSelected ? '2px solid #C2EE73' : 'none')};
+
+  /* 온보딩그림자 */
+  box-shadow:
+    0 0 8px 0 rgba(3, 3, 3, 0.05),
+    0 0 30px 0 rgba(3, 3, 3, 0.05);
+  color: var(--900, #030303);
+  font-family: 'Wanted Sans Variable';
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 120%; /* 18px */
+`;
+const Icon = styled.img``;
+const XBtn = styled.img`
+  margin-right: 2px;
+`;
+const Amount = styled.div`
+  color: var(--500, #5a5a5b);
+  font-family: 'Wanted Sans Variable';
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 120%; /* 14.4px */
+`;
